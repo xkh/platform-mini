b234e9315a49eff05bc35677c1e91a9ac9245948
xingkaihang <xingkaihang@aliyun.com>
2020-07-27 03:02:05 +0800

    preload

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+    // browser-friendly UMD build
+    {
+        input: 'src/preloadAll.js',
+        output: {
+            name: 'preload-all',
+            file: pkg.browser,
+            format: 'umd',
+        },
+        plugins: [
+            resolve(), // so Rollup can find `ms`
+            commonjs(), // so Rollup can convert `ms` to an ES module
+            process.env.NODE_ENV === 'production' && terser(),
+        ],
+    },
+
+    // CommonJS (for Node) and ES module (for bundlers) build.
+    // (We could have three entries in the configuration array
+    // instead of two, but it's quicker to generate multiple
+    // builds from a single configuration where possible, using
+    // an array for the `output` option, where we can specify
+    // `file` and `format` for each target)
+    // {
+    //     input: 'src/preloadAll.js',
+    //     // external: ['排除第三方'],
+    //     output: [
+    //         { file: pkg.main, format: 'cjs' },
+    //         { file: pkg.module, format: 'es' },
+    //     ],
+    // },
+];
